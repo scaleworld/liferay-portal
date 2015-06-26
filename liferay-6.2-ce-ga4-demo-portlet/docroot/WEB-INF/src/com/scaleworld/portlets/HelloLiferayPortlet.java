e39badc630a99d932ea37153ef4044202b6bdc9f
scaleworld <scaleworld@qq.com>
2015-06-26 08:16:17 +0800

    基于liferay 6.2社区GA4版本开发的示例插件工程
    包含以下portlet：
    1、默认生成的示例portlet：liferay-6.2-ce-ga4-demo
    2、向导生成的简单示例portlet：hello-liferay

---
+++ liferay-6.2-ce-ga4-demo-portlet/docroot/WEB-INF/src/com/scaleworld/portlets/HelloLiferayPortlet.java
@@ -0,0 +1,11 @@
+package com.scaleworld.portlets;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class HelloLiferayPortlet
+ */
+public class HelloLiferayPortlet extends MVCPortlet {
+ 
+
+}
